1fdfbd9ddaf1aea999c32596f5ec6fdcd4561004
AnatoliyGG <tolik-mazkov@mail.ru>
2020-02-03 02:41:06 +0300

    asd

--- LessonsOne/Form1.cs
+++ LessonsOne/Form1.cs
@@ -1,20 +1,34 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace LessonsOne
 {
     public partial class Form1 : Form
     {
+        string File1 = @"C:\Users\AnatoliyGG\Desktop\1.cmd";
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (File.Exists(File1))
+            {
+                Method("Файл есть");
+            }
+            else
+            {
+                Method("Файла нет");
+            }
+        }
+        
+        void Method(string line)
+        {
+            MessageBox.Show(line, "Уведомление", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+
         }
     }
 }
