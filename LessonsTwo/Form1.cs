06b69917ae21b634dc0cbe5a3d267f1a9b7ffe2b
AnatoliyGG <tolik-mazkov@mail.ru>
2020-02-04 01:12:28 +0300

    Date

---
+++ LessonsTwo/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace LessonsTwo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = $"{Hello()}, Юзернейм";
+        }
+
+        string Hello()
+        {
+            var time = DateTime.Now.Hour;
+            if (time > 3)
+            {
+                return $"Доброе раннее утро";
+            }
+            else if (time < 12)
+            {
+                return $"Доброе  утро";
+            }
+            else if (time < 15)
+            {
+                return $"Доброе раннее утро";
+            }
+            else
+            {
+                return "Добрый обед";
+            }
+        }
+    }
+}
